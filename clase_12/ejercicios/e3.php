4db91f002833681d1629b323d3744e1ae337a872
Guille <cochraneguillermo@gmail.com>
2026-08-13 20:24:34 -0300

    Agregado Ejercicio de tareas 3, de la clase 12

---
+++ clase_12/ejercicios/e3.php
@@ -0,0 +1,20 @@
+<?php
+    $nota_final = 7;
+    $asistencia = 85;
+    
+    // 1. Evalúa si la nota es mayor o igual a 7 Y la asistencia es mayor o igual a 80
+    if ($nota_final >= 7  && $asistencia >= 80) {
+        
+        echo "Aprobado y promocionado";
+        
+    // 2. Evalúa si la nota es mayor o igual a 4 (condición alternativa)
+    } elseif ($nota_final >= 4) { 
+        
+        echo "Aprobado, rinde final";
+        
+    } else {
+        
+        echo "Recursa la materia";
+        
+    }
+?>
