91655cd4dd6851cdc4b51beb0b129b6351c9595b
Guille <cochraneguillermo@gmail.com>
2026-08-21 18:44:11 -0300

    Agregada resolucion del inciso 1 del ejercicio de tarea de la clase 13

---
+++ clase_13/ejercicios/integrador_profes/alumnos.php
@@ -0,0 +1,51 @@
+<?php
+  /*
+    👨‍🎓 *1. Crear un array de alumnos*
+
+      En `alumnos.php` deberán crear un array que contenga varios alumnos.
+
+      Cada alumno debe tener:
+
+      * Nombre
+      * Nota 1
+      * Nota 2
+      * Asistencia
+
+      Ejemplo de datos:
+
+      Lucio → Nota 1: 8 | Nota 2: 9 | Asistencia: 90%
+      Juan → Nota 1: 6 | Nota 2: 5 | Asistencia: 75%
+      Ana → Nota 1: 3 | Nota 2: 4 | Asistencia: 85%
+      Martina → Nota 1: 9 | Nota 2: 8 | Asistencia: 60%
+  */
+  $alumnos = [
+    [
+      'id' => 1,
+      'nombre' => 'Lucio',
+      'nota1' => 8,
+      'nota2' => 9,
+      'asistencia' => 90
+    ],
+    [
+      'id' => 2,
+      'nombre' => 'Juan',
+      'nota1' => 6,
+      'nota2' => 5,
+      'asistencia' => 75
+    ],
+    [
+      'id' => 3,
+      'nombre' => 'Ana',
+      'nota1' => 3,
+      'nota2' => 4,
+      'asistencia' => 85
+    ],
+    [
+      'id' => 4,
+      'nombre' => 'Martina',
+      'nota1' => 9,
+      'nota2' => 8,
+      'asistencia' => 60
+    ]
+  ];
+?>
