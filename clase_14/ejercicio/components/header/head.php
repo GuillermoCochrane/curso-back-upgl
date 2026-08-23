198b25c18af91964468865fd2e42f6598a5ce783
Guille <cochraneguillermo@gmail.com>
2026-08-23 20:16:50 -0300

    Agregado componente del head

---
+++ clase_14/ejercicio/components/header/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  <link href="https://upgl.vercel.app/images/icon.png" rel="icon" type="image/png" />
+  <title><?= isset($title) ? $title : 'Pagina X' ?></title>
+  <style>
+    body {
+      margin: 0;
+      background-color: #f4f1de;
+      color: #2f3e46;
+      border-top: 6px solid #264653;
+      border-bottom: 6px solid #264653;
+      min-height: 100vh;
+    }
+
+    header {
+      padding: 24px;
+      background-color: #2a9d8f;
+      border-top: 4px solid #1d6f66;
+      border-bottom: 4px solid #101111;
+      color: #ffffff;
+    }
+
+    footer {
+      padding: 24px;
+      background-color: #ff3300;
+      border-top: 4px solid #b94f38;
+      border-bottom: 4px solid #b94f38;
+      color: #ffffff;
+    }
+  </style>
+</head>
