44464ee28a39596034bc2fad19993e96c49512e1
Guille <cochraneguillermo@gmail.com>
2026-08-27 14:13:11 -0300

    Agregado orquestador de pagina 2

--- clase_14/ejercicio/pages/pagina2/index.php
+++ clase_14/ejercicio/pages/pagina2/index.php
@@ -1,5 +1,11 @@
-<?php include_once '../../components/header/head.php'; ?>
-<?php include_once '../../components/header/header.php'; ?>
-    <h1>PAGINA 2 DEL BODY</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, officia vitae soluta voluptas aut et maiores fugiat quisquam dolorem totam ab excepturi nesciunt laborum eius omnis repellat ad, aperiam dolore.</p>
-<?php include_once '../../components/footer/footer.php'; ?>
+<?php 
+  $title = 'PAGINA 2';
+  $home_link = '../../';
+  $home_tag = 'Home';
+  $page1_link = '../pagina1/';
+  $page1_tag = 'PAGINA 1';
+  $page2_link = './';
+  $page2_tag = 'PAGINA 2';
+?>
+
+<?php include './template.php'; ?>
