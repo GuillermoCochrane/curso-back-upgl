3c8155aab86e41fa5287951f1c979028045739a4
Guille <cochraneguillermo@gmail.com>
2026-08-27 14:40:56 -0300

    Agregado componente de item navbar

---
+++ clase_14/ejercicio/components/header/nav_item.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+  <a 
+    class="nav-link <?= isset($active) ? $active : '' ?>"
+    href="<?= isset($page_link) ? $page_link : '#' ?>
+    ">
+    <?= isset($page_tag) ? $page_tag : 'PAGINA X' ?>
+  </a>
+</li>
