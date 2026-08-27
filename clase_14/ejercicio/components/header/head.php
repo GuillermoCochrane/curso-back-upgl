78c5110da02a9975aa26c5ca346c9e8efecdd7e5
Guille <cochraneguillermo@gmail.com>
2026-08-27 14:09:22 -0300

    Modularizados estilos del ejericio

--- clase_14/ejercicio/components/header/head.php
+++ clase_14/ejercicio/components/header/head.php
@@ -7,30 +7,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   <link href="https://upgl.vercel.app/images/icon.png" rel="icon" type="image/png" />
   <title><?= isset($title) ? $title : 'Pagina X' ?></title>
-  <style>
-    body {
-      margin: 0;
-      background-color: #f4f1de;
-      color: #2f3e46;
-      border-top: 6px solid #264653;
-      border-bottom: 6px solid #264653;
-      min-height: 100vh;
-    }
-
-    header {
-      padding: 24px;
-      background-color: #2a9d8f;
-      border-top: 4px solid #1d6f66;
-      border-bottom: 4px solid #101111;
-      color: #ffffff;
-    }
-
-    footer {
-      padding: 24px;
-      background-color: #ff3300;
-      border-top: 4px solid #b94f38;
-      border-bottom: 4px solid #b94f38;
-      color: #ffffff;
-    }
-  </style>
+  <?php include "style.php"; ?>
 </head>
