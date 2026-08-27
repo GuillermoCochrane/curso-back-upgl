7319fdffec03e90b3439d8ca56171bbb12ca7b2a
Guille <cochraneguillermo@gmail.com>
2026-08-27 14:11:50 -0300

    Agregado template de pagina 1

---
+++ clase_14/ejercicio/pages/pagina1/template.php
@@ -0,0 +1,5 @@
+<?php include_once '../../components/header/head.php'; ?>
+<?php include_once '../../components/header/header.php'; ?>
+    <h1>PAGINA 1 DEL BODY </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, officia vitae soluta voluptas aut et maiores fugiat quisquam dolorem totam ab excepturi nesciunt laborum eius omnis repellat ad, aperiam dolore.</p>
+<?php include_once '../../components/footer/footer.php'; ?>
