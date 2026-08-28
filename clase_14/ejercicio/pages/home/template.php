495f750209cf32e1a22c54f8c1ab5df673a63711
Guille <cochraneguillermo@gmail.com>
2026-08-28 19:48:07 -0300

    Agregado template de home

---
+++ clase_14/ejercicio/pages/home/template.php
@@ -0,0 +1,9 @@
+<?php include_once 'components/header/head.php'; ?>
+<?php include_once 'components/header/header.php'; ?>
+    
+  <h1 class="text-center">Este es el Home del sitio</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, officia vitae soluta voluptas aut et maiores fugiat quisquam dolorem totam ab excepturi nesciunt laborum eius omnis repellat ad, aperiam dolore.</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, officia vitae soluta voluptas aut et maiores fugiat quisquam dolorem totam ab excepturi nesciunt laborum eius omnis repellat ad, aperiam dolore.</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, officia vitae soluta voluptas aut et maiores fugiat quisquam dolorem totam ab excepturi nesciunt laborum eius omnis repellat ad, aperiam dolore.</p>
+
+<?php include_once 'components/footer/footer.php'; ?>
