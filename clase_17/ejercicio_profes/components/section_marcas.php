6ee6969df1c5a03e940ca6b36079a01b7941abfa
Guille <cochraneguillermo@gmail.com>
2026-09-14 17:25:31 -0300

    Agregada seccion de marca

---
+++ clase_17/ejercicio_profes/components/section_marcas.php
@@ -0,0 +1,16 @@
+<div 
+  id="<?= isset($marca) ? $marca : "marca-generica" ?>" 
+  class="marca-seccion pb-5 mb-4"
+  >
+    <h2 class="border-start border-4 border-warning ps-3 mb-4 fw-bold h3">
+      <?= isset($marca) ? $marca : "Marca Generica" ?>
+    </h2>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
+      <?php
+        foreach (isset($modelos) ? $modelos : [] as $modelo) {
+          extract($modelo);
+          include("card_zapatillas.php");
+        } 
+      ?>
+    </div>
+</div>
