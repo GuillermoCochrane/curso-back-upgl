04e8ea4cbed73e415af10d7f3a1d95c45087b5f6
Guille <cochraneguillermo@gmail.com>
2026-09-14 15:33:53 -0300

    Agregado Ejercicio hecho en clase 17 por los profes

---
+++ clase_17/ejercicio_profes/index.php
@@ -0,0 +1,37 @@
+<?php
+/* 📌 TRABAJO INTEGRADOR – PHP NIVEL INTERMEDIO
+Objetivo:
+Crea un ABM que permita agregar-modificar-borrar zapatillas y mostrarlas en una pagina...
+
+📂 El proyecto deberá tener estos archivos:
+
+- header.php
+- footer.php
+- funciones.php
+- ABM_zapatillas.php
+- tienda_de_zapatillas.php
+- index.php
+Cada zapatilla tiene que tener:
+- Marca
+- Modelo
+- Color
+- Talle
+- Precio
+- Stock
+
+ */
+$marca1 = "Nike";
+$marca2 = "Adidas";
+$marca3 = "Puma";
+$marca4 = "Reebok";
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<?php include_once("components/header.php"); ?>
+<?php include_once("components/navbar.php"); ?>
+<?php include_once("pages/tienda_de_zapatillas.php"); ?>
+<?php /* include_once("ABM_zapatillas.php"); */ ?>
+<?php include_once("components/footer.php"); ?>
+
+</html>
---
+++ clase_17/ejercicio_profes/pages/titulo_marca_zapa.php
@@ -0,0 +1,3 @@
+ <h2 class="border-start border-4 border-warning ps-3 mb-4 fw-bold h3">
+    <?php echo $marca; ?>
+</h2>
